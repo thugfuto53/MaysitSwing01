5c84a49204148f6ca87e28b6962150c4edad7102
thugfuto53 <86662632+thugfuto53@users.noreply.github.com>
2021-07-28 15:53:40 +0900

    Add files via upload

---
+++ Uri.java
@@ -0,0 +1,12 @@
+public class Uri{
+        static String uriString0 = "http://www.google.co.jp";
+        static String uriString1 = "https://www.youtube.com/";
+        static String uriString2 = "https://www.google.com/maps/@36.9370426,139.9059505,14z";
+        static String uriString3 = "https://calendar.google.com/calendar/u/0/r?pli=1";
+        static String uriString4 = "https://mail.google.com/mail/u/0/#inbox";
+        static String uriString5 = "https://chrome.google.com/webstore/category/extensions?hl=ja";
+        static String uriString6 = "https://drive.google.com/drive/my-drive";
+        static String uriString7 = "https://docs.google.com/spreadsheets/u/0/";
+        static String uriString8 = "https://docs.google.com/document/u/0/";
+        static String uriString9 = "https://github.com/";
+}
